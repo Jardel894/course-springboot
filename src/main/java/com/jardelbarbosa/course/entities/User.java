7e48e29e30a125a5b1ce38bc3fd8ef42b8b3ad14
Jardel894 <jardelsantos894@gmail.com>
2022-06-27 09:43:33 -0300

    H2 database, test profile and JPA

--- src/main/java/com/jardelbarbosa/course/entities/User.java
+++ src/main/java/com/jardelbarbosa/course/entities/User.java
@@ -1,15 +1,24 @@
 package com.jardelbarbosa.course.entities;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
-@EqualsAndHashCode
-public class User {
+@Entity
+@Table(name = "users")
+public class User{
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
     private String name;
     private String email;
     private String phone;
