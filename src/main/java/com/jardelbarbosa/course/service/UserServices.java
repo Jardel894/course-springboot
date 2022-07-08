fd3c6bae09ac66c9d37a4270e91a5a60185940fc
Jardel894 <jardelsantos894@gmail.com>
2022-07-08 13:58:39 -0300

    User insert

--- src/main/java/com/jardelbarbosa/course/service/UserServices.java
+++ src/main/java/com/jardelbarbosa/course/service/UserServices.java
@@ -23,4 +23,8 @@ public class UserServices {
         Optional<User> obj = userRepository.findById(id);
         return obj.get();
     }
+
+    public User isert(User obj){
+        return userRepository.save(obj);
+    }
 }
